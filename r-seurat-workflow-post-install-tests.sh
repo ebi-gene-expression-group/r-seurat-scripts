2de4a950adad49b26e6b1705dbdbd68f51b54099
Jonathan Manning <jmanning@ebi.ac.uk>
2018-07-05 15:28:07 +0100

    Centralised command running and checking

--- r-seurat-workflow-post-install-tests.sh
+++ r-seurat-workflow-post-install-tests.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
 function usage {
-    echo "usage: r-seurat-workflow-post-install-tests.sh [action]"
-    echo "  -action: what action to take, 'test' or 'clean'"
+    echo "usage: r-seurat-workflow-post-install-tests.sh [action] [use_existing_outputs]"
+    echo "  - action: what action to take, 'test' or 'clean'"
+    echo "  - use_existing_outputs, 'true' or 'false'"
     exit 1
 }
 
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
+action=${1:-'test'}
+use_existing_outputs=${2:-'false'}
+
+if [ "$action" != 'test' ] && [ "$action" != 'clean' ]; then
+    echo "Invalid action"
     usage
 fi
 
-action=$1
+if [ "$use_existing_outputs" != 'true' ] && [ "$use_existing_outputs" != 'false' ]; then
+    echo "Invalid value ($use_existing_outputs) for 'use_existing_outputs'"
+    usage
+fi
 
 test_data_url='https://s3-us-west-2.amazonaws.com/10x.files/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz'
-test_data_archive=`basename $test_data_url`
-test_working_dir='post_install_tests'
+test_working_dir=`pwd`/'post_install_tests'
+test_data_archive=$test_working_dir/`basename $test_data_url`
 
 # Clean up if specified
 
@@ -30,8 +37,12 @@ fi
 
 # Initialise directories
 
+output_dir=$test_working_dir/outputs
+data_dir=$test_working_dir/test_data
+
 mkdir -p $test_working_dir
-mkdir -p $test_working_dir/outputs
+mkdir -p $output_dir
+mkdir -p $data_dir
 
 cd $test_working_dir
 
@@ -42,9 +53,6 @@ cd $test_working_dir
 if [ ! -e "$test_data_archive" ]; then
     wget $test_data_url
 fi
-
-echo "Extracting test data from archive"
-mkdir -p test_data && tar -xvzf $test_data_archive --strip-components 2 -C test_data
 
 ################################################################################
 # Accessory functions
@@ -62,32 +70,71 @@ function report_status() {
     fi
 }
 
+# Run a command, checking the primary output depending on the value of
+# 'use_existing_outputs'
+
+run_command() {
+    command=$1
+    test_output=$2
+
+    echo "$command"
+    command_name=`echo "$command" | awk '{print $1}'`
+
+    if [ -e "$test_output" ] && [ "$use_existing_outputs" == "true" ]; then
+        echo "Using cached output for $command_name"
+    else
+        eval $command
+        report_status $command_name $?
+    fi
+}
+
 ################################################################################
 # List tool outputs/ inputs
 ################################################################################
 
-raw_matrix_object='outputs/raw_matrix.rds'
-raw_seurat_object='outputs/raw_seurat.rds'
-filtered_seurat_object='outputs/filtered_seurat.rds'
+raw_matrix='test_data/matrix.mtx'
+raw_matrix_object="$output_dir/raw_matrix.rds"
+raw_seurat_object="$output_dir/raw_seurat.rds"
+filtered_seurat_object="$output_dir/filtered_seurat.rds"
+normalised_seurat_object="$output_dir/normalised_seurat.rds"
+
+## Test parameters- would form config file in real workflow
+
+# Normalisation. See Seurat ?FilterCells
+
+min_genes=500
+min_umi=1000
+
+# Normalisation. See Seurat ?NormalizeData
+
+assay_type='RNA'
+normalisation_method='LogNormalize'
+scale_factor=10000
 
 ################################################################################
 # Test individual scripts
 ################################################################################
 
+# Extract the test data
+
+echo "Extracting test data from archive"
+run_command "tar -xvzf $test_data_archive --strip-components 2 -C test_data" $raw_matrix
+
 # Run read-10x.R
 
-read-10x.R -d test_data -o $raw_matrix_object
-report_status read-10x.R $?
+run_command "read-10x.R -d test_data -o $raw_matrix_object" $raw_matrix_object
 
 # Run create-seurat-object.R
 
-create-seurat-object.R -i $raw_matrix_object -o $raw_seurat_object
-report_status create-seurat-object.R $?
+run_command "create-seurat-object.R -i $raw_matrix_object -o $raw_seurat_object" $raw_seurat_object
 
 # Run filter-cells.R
 
-filter-cells.R -i $raw_seurat_object -s nGene,nUMI -l 500,1000 -o $filtered_seurat_object
-report_status filter-cells.R $?
+run_command "filter-cells.R -i $raw_seurat_object -s nGene,nUMI -l $min_genes,$min_umi -o $filtered_seurat_object" $filtered_seurat_object
+
+# Run normalise-date.R
+
+run_command "normalise-data.R -i $filtered_seurat_object -a $assay_type -n $normalisation_method -s $scale_factor -o $normalised_seurat_object" $normalised_seurat_object
 
 ################################################################################
 # Finish up
