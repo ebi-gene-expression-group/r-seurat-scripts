704875f1f6591c1f63c8ea922c250dbaebc694d6
Jonathan Manning <jmanning@ebi.ac.uk>
2018-07-05 09:42:46 +0100

    Create separate actions and add filter-cells.R

--- r-seurat-workflow-post-install-tests.sh
+++ r-seurat-workflow-post-install-tests.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
+function usage {
+    echo "usage: r-seurat-workflow-post-install-tests.sh [action]"
+    echo "  -action: what action to take, 'test' or 'clean'"
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    usage
+fi
+
+action=$1
+
 test_data_url='https://s3-us-west-2.amazonaws.com/10x.files/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz'
 test_data_archive=`basename $test_data_url`
 test_working_dir='post_install_tests'
+
+# Clean up if specified
+
+if [ "$action" = 'clean' ]; then
+    echo "Cleaning up $test_working_dir ..."
+    rm -rf $test_working_dir
+    exit 0
+elif [ "$action" != 'test' ]; then
+    echo "Invalid action '$action' supplied"
+    exit 1
+fi 
+
+# Initialise directories
 
 mkdir -p $test_working_dir
 mkdir -p $test_working_dir/outputs
@@ -42,6 +68,7 @@ function report_status() {
 
 raw_matrix_object='outputs/raw_matrix.rds'
 raw_seurat_object='outputs/raw_seurat.rds'
+filtered_seurat_object='outputs/filtered_seurat.rds'
 
 ################################################################################
 # Test individual scripts
@@ -57,12 +84,14 @@ report_status read-10x.R $?
 create-seurat-object.R -i $raw_matrix_object -o $raw_seurat_object
 report_status create-seurat-object.R $?
 
+# Run filter-cells.R
+
+filter-cells.R -i $raw_seurat_object -s nGene,nUMI -l 500,1000 -o $filtered_seurat_object
+report_status filter-cells.R
+
 ################################################################################
 # Finish up
 ################################################################################
 
 echo "All tests passed"
-echo "Cleaning up..."
-cd ..
-rm -rf $test_working_dir
 exit 0
