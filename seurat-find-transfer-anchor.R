9daa330b6a007d160acdbd041b90f112057df3f8
Guilhem Marnier <magu3895@noah-login-01.ebi.ac.uk>
2020-03-18 12:23:29 +0000

    all until l2.norm

--- seurat-find-transfer-anchor.R
+++ seurat-find-transfer-anchor.R
@@ -199,8 +199,8 @@ anchor_object <- FindTransferAnchors(seurat_reference,
                                     reduction = opt$reduction,
                                     project.query = opt$project_query,
                                     features = opt$features,
-                                    npcs = opt$npcs)
-                      #              l2.norm = opt$l2_norm,
+                                    npcs = opt$npcs,
+                                    l2.norm = opt$l2_norm)
                        #             dims = opt$dims,
                         #            k.anchor = opt$k_anchor,
                          #           k.filter = opt$k_filter,
