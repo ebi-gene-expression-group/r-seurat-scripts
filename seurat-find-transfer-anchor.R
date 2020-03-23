5254c495d62e5fedcf5a4301309402bcd39a1d94
Guilhem Marnier <magu3895@noah-login-03.ebi.ac.uk>
2020-03-23 12:11:32 +0000

    change the part that convert dims

--- seurat-find-transfer-anchor.R
+++ seurat-find-transfer-anchor.R
@@ -165,11 +165,12 @@ opt <- wsc_parse_args(option_list, mandatory = c('query_file', 'reference_file',
 if ( ! file.exists(opt$reference_file)){
   stop((paste('File', opt$reference_file, 'does not exist')))
 
+
+#convert dims from "a:b" to a real vector
 dims <- opt$dims
-if ( ! is.null(dims)){
-  dims <- strsplit(dims, ":")
-  dims <- seq(strtoi(dims[0]),strtoi(dims[1]),1) 
-}
+dims <- strsplit(dims, ":")
+dims <- seq(strtoi(dims[0]),strtoi(dims[1]),1) 
+
 
 
 
@@ -200,7 +201,7 @@ anchor_object <- FindTransferAnchors(seurat_reference,
                                     features = opt$features,
                                     npcs = opt$npcs,
                                     l2.norm = opt$l2_norm,
-                                    dims = dims,
+                                    dims,
                                     k.anchor = opt$k_anchor,
                                     k.filter = opt$k_filter,
                                     k.score = opt$k_score,
