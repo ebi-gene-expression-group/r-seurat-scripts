8800ee2658549085745304aa03a158a22aab0f82
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-10-28 17:58:06 +0100

    Remove old flags in fvg and use manual seurat-convert script.

--- seurat-convert.R
+++ seurat-convert.R
@@ -1,58 +1,67 @@
 #!/usr/bin/env Rscript
-# This script has been automatically generated through
-#
-# YAML2RScript.py -i ../r-seurat-scripts/cli-generated/manually_crafted_YAML/seurat-convert.yaml -o ../r-seurat-scripts/seurat-convert.R
-#
-# to change this file edit the input YAML and re-run the above command
 
-suppressPackageStartupMessages(require(Seurat))
-suppressPackageStartupMessages(require(loomR))
+# Load optparse we need to check inputs
+
 suppressPackageStartupMessages(require(optparse))
-suppressPackageStartupMessages(require(scater))
+
+# Load common functions
+
 suppressPackageStartupMessages(require(workflowscriptscommon))
 
-option_list <- list(
-    make_option(
-                    c("-i", "--input-path"),
-                    action = "store",
-                    metavar = "Input file",
-                    type = "character",
-                    help = "Input file in either RDS-Seurat, Loom, AnnData or SCE")
-,
-    make_option(
-                    c("--input-format"),
-                    action = "store",
-                    default = "seurat",
-                    metavar = "Input format",
-                    type = "character",
-                    help = "Either loom, seurat, anndata or singlecellexperiment for the input format to read.")
-,
-    make_option(
-                    c("-o", "--output-path"),
-                    action = "store",
-                    type = "character",
-                    help = "Path to the output file, when using Loom as output, the final file will contain .loom at the end of the given file name.")
-,
-    make_option(
-                    c("--output-format"),
-                    action = "store",
-                    default = "seurat",
-                    type = "character",
-                    help = "Either seurat, loom, singlecellexperiment or h5seurat (partial support)")
+# parse options
+
+option_list = list(
+  make_option(
+    c("-i", "--input-object-file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "File name in which a serialized R matrix object may be found."
+  ),
+  make_option(
+    c("--input-format"),
+    action = "store",
+    default = "seurat",
+    type = 'character',
+    help = "Either loom, seurat, anndata or singlecellexperiment for the input format to read."
+  ),
+  make_option(
+    c("--output-format"),
+    action = "store",
+    default = "seurat",
+    type = 'character',
+    help = "Either loom, seurat, anndata or singlecellexperiment for the output format."
+  ),
+  make_option(
+    c("-o", "--output-object-file"),
+    action = "store",
+    default = NA,
+    type = 'character',
+    help = "File name in which to store serialized R object of type 'Seurat'.'"
+  )
 )
 
-opt <- wsc_parse_args(option_list, 
-                      mandatory = c("input_path", "output_path"))
+opt <- wsc_parse_args(option_list, mandatory = c('input_object_file', 'output_object_file'))
 
+# Check parameter values
 
-if (!file.exists(opt$input_path)) {
-    stop((paste("File", opt$input_path, "does not exist")))
+if ( ! file.exists(opt$input_object_file)){
+  stop((paste('File', opt$input_object_file, 'does not exist')))
 }
 
+# Now we're hapy with the arguments, load Seurat and do the work
 
-seurat_object <- read_seurat3_object(input_path = opt$input_path,
-                    format = opt$input_format)
+suppressPackageStartupMessages(require(Seurat))
+if(opt$input_format == "loom" | opt$output_format == "loom") {
+  suppressPackageStartupMessages(require(loomR))
+} else if(opt$input_format == "singlecellexperiment" | opt$output_format == "singlecellexperiment") {
+  suppressPackageStartupMessages(require(scater))
+}
 
+# Input from serialized R object
+seurat_object <- read_seurat3_object(input_path = opt$input_object_file, format = opt$input_format)
+
+# Output to a serialized R object
 write_seurat3_object(seurat_object = seurat_object,
-                    output_path = opt$output_path,
-                    format = opt$output_format)
+                     output_path = opt$output_object_file,
+                     format = opt$output_format)
