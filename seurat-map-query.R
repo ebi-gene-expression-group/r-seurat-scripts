fdd72a27938d42dde4c540aec8e25690229833c7
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-11-30 22:09:27 +0000

    Map query automatic changes

--- seurat-map-query.R
+++ seurat-map-query.R
@@ -1,128 +1,129 @@
 #!/usr/bin/env Rscript
 # This script has been automatically generated through
 #
-# YAML2RScript.py -i ../r-seurat-scripts/cli-generated/manually_crafted_YAML/seurat-map-query.yaml -o ../r-seurat-scripts/seurat-map-query.R
+# YAML2RScript.py -i ../r-seurat-scripts/cli-generated/manually_crafted_YAML/seurat-map-query.yaml -o ../r-seurat-scripts/cli-generated/auto-generated-R/seurat-map-query.R
 #
 # to change this file edit the input YAML and re-run the above command
 
-suppressPackageStartupMessages(require(SeuratDisk))
 suppressPackageStartupMessages(require(optparse))
-suppressPackageStartupMessages(require(workflowscriptscommon))
 suppressPackageStartupMessages(require(Seurat))
+suppressPackageStartupMessages(require(SeuratDisk))
 suppressPackageStartupMessages(require(scater))
+suppressPackageStartupMessages(require(workflowscriptscommon))
 
 option_list <- list(
     make_option(
-                    c("-q", "--query-path"),
-                    action = "store",
-                    metavar = "Input file",
-                    type = "character",
-                    help = "Query file with Seurat object in either RDS-Seurat, Loom or SCE")
-,
+        c("-q", "--query-path"),
+        action = "store",
+        metavar = "Input file",
+        type = "character",
+        help = "Query file with Seurat object in either RDS-Seurat, Loom or SCE"
+    ),
     make_option(
-                    c("--query-format"),
-                    action = "store",
-                    default = "seurat",
-                    metavar = "Input format",
-                    type = "character",
-                    help = "Either loom, seurat, anndata or singlecellexperiment for the input format to read.")
-,
+        c("--query-format"),
+        action = "store",
+        default = "seurat",
+        metavar = "Input format",
+        type = "character",
+        help = "Either loom, seurat, anndata or singlecellexperiment for the input format to read."
+    ),
     make_option(
-                    c("-a", "--anchors-path"),
-                    action = "store",
-                    metavar = "Seurat transfer anchors file",
-                    type = "character",
-                    help = "Input file with Seurat object with anchors in either RDS-Seurat, Loom or SCE")
-,
+        c("-a", "--anchors-path"),
+        action = "store",
+        metavar = "Seurat transfer anchors file",
+        type = "character",
+        help = "Input file with Seurat object with anchors in either RDS-Seurat, Loom or SCE"
+    ),
     make_option(
-                    c("--anchors-format"),
-                    action = "store",
-                    default = "seurat",
-                    metavar = "Anchors format",
-                    type = "character",
-                    help = "Either loom, seurat, anndata or singlecellexperiment for the anchors format to read.")
-,
+        c("--anchors-format"),
+        action = "store",
+        default = "seurat",
+        metavar = "Anchors format",
+        type = "character",
+        help = "Either loom, seurat, anndata or singlecellexperiment for the anchors format to read."
+    ),
     make_option(
-                    c("-r", "--reference-path"),
-                    action = "store",
-                    default = NULL,
-                    metavar = "Seurat reference object file",
-                    type = "character",
-                    help = "Input file with Seurat object with reference (and UMAP) in either RDS-Seurat, Loom or SCE")
-,
+        c("-r", "--reference-path"),
+        action = "store",
+        default = NULL,
+        metavar = "Seurat reference object file",
+        type = "character",
+        help = "Input file with Seurat object with reference (and UMAP) in either RDS-Seurat, Loom or SCE"
+    ),
     make_option(
-                    c("--reference-format"),
-                    action = "store",
-                    default = "seurat",
-                    metavar = "Anchors format",
-                    type = "character",
-                    help = "Either loom, seurat, anndata or singlecellexperiment for the reference format to read.")
-,
+        c("--reference-format"),
+        action = "store",
+        default = "seurat",
+        metavar = "Anchors format",
+        type = "character",
+        help = "Either loom, seurat, anndata or singlecellexperiment for the reference format to read."
+    ),
     make_option(
-                    c("--refdata-field-or-assay"),
-                    action = "store",
-                    type = "character",
-                    help = "The name of the metadata field or assay from the reference object provided. This requires the reference parameter to be specified.")
-,
+        c("--refdata-field-or-assay"),
+        action = "store",
+        type = "character",
+        help = "The name of the metadata field or assay from the reference object provided. This requires the reference parameter to be specified."
+    ),
     make_option(
-                    c("--new-reduction-name"),
-                    action = "store",
-                    default = NULL,
-                    type = "character",
-                    help = "Name for new integrated dimensional reduction.")
-,
+        c("--new-reduction-name"),
+        action = "store",
+        default = NULL,
+        type = "character",
+        help = "Name for new integrated dimensional reduction."
+    ),
     make_option(
-                    c("--reference-reduction"),
-                    action = "store",
-                    default = NULL,
-                    type = "character",
-                    help = "Name of reduction to use from the reference for neighbor finding")
-,
+        c("--reference-reduction"),
+        action = "store",
+        default = NULL,
+        type = "character",
+        help = "Name of reduction to use from the reference for neighbor finding"
+    ),
     make_option(
-                    c("--reduction-model"),
-                    action = "store",
-                    default = NULL,
-                    type = "character",
-                    help = "DimReduc object name that contains the umap model")
-,
+        c("--reduction-model"),
+        action = "store",
+        default = NULL,
+        type = "character",
+        help = "DimReduc object name that contains the umap model"
+    ),
     make_option(
-                    c("--transferdata-args"),
-                    action = "store",
-                    default = NULL,
-                    type = "character",
-                    help = "A named list of additional arguments to TransferData, written in R syntax .ie list( argument = 'value' )")
-,
+        c("--transferdata-args"),
+        action = "store",
+        default = NULL,
+        type = "character",
+        help = "A named list of additional arguments to TransferData, written in R syntax .ie list( argument = 'value' )"
+    ),
     make_option(
-                    c("--integrated-embedding-args"),
-                    action = "store",
-                    default = NULL,
-                    type = "character",
-                    help = "A named list of additional arguments to IntegrateEmbeddings, written in R syntax .ie list( argument = 'value' )")
-,
+        c("--integrated-embedding-args"),
+        action = "store",
+        default = NULL,
+        type = "character",
+        help = "A named list of additional arguments to IntegrateEmbeddings, written in R syntax .ie list( argument = 'value' )"
+    ),
     make_option(
-                    c("--project-umap-args"),
-                    action = "store",
-                    default = NULL,
-                    type = "character",
-                    help = "A named list of additional arguments to ProjectUMAP, written in R syntax .ie list( argument = 'value' )")
-,
+        c("--project-umap-args"),
+        action = "store",
+        default = NULL,
+        type = "character",
+        help = "A named list of additional arguments to ProjectUMAP, written in R syntax .ie list( argument = 'value' )"
+    ),
     make_option(
-                    c("-o", "--output-path"),
-                    action = "store",
-                    type = "character",
-                    help = "FILE IN")
-,
+        c("-o", "--output-path"),
+        action = "store",
+        type = "character",
+        help = "FILE IN"
+    ),
     make_option(
-                    c("--output-format"),
-                    action = "store",
-                    default = "seurat",
-                    type = "character",
-                    help = "FILE IN")
+        c("--output-format"),
+        action = "store",
+        default = "seurat",
+        type = "character",
+        help = "Either loom, seurat, anndata or singlecellexperiment for the anchors format to read.'"
+    )
 )
 
 opt <- wsc_parse_args(option_list, 
                       mandatory = c("query_path", "anchors_path", "refdata_field_or_assay", "output_path"))
-
+                
 
 if (!file.exists(opt$query_path)) {
     stop((paste("File", opt$query_path, "does not exist")))
@@ -164,6 +165,8 @@ if (!is.null(opt$project_umap_args)) {
     opt$project_umap_args <- eval(parse(text = opt$project_umap_args))
 }
 
+
+load_seurat4_packages_for_format(formats = c(opt$query_format, opt$anchors_format, opt$reference_format))
 
 seurat_object <- read_seurat4_object(input_path = opt$query_path,
                     format = opt$query_format)
